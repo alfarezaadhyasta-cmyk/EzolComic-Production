fbf3b59dded94e398ad8ee3d8cf93a59a6251573
alfarezaadhyasta-cmyk <alfarezaadhyasta@gmail.com>
2026-05-31 19:44:11 +0700

    Add click event to tombol_about for alert message

---
+++ script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("tombol_about").addEventListener("click", function() {
+    alert("Hai! Aku Adhyasta Khairy Alfareza, sering dipanggil Eza/Ezol. Aku hobi gambar komik lho! Cuma sekarang jarang ngomik sih... karena terlalu sibuk dengan keadaan dan yaa... jujur ada malesnya! :D")
+});
